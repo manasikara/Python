23bc998ae3119ef1d0740c9a87628935103f84e6
slawomir_tatinger <59393759+manasikara@users.noreply.github.com>
2023-08-08 10:41:27 +0200

    ...

---
+++ Python_turtle_game_dev/pong.py
@@ -0,0 +1,27 @@
+# just a Pong game. Source --> https://www.youtube.com/watch?v=XGf2GcyHPhc&t=184s
+
+
+import turtle
+import tkinter
+
+wn = turtle.Screen()
+wn.title("Pong by Statinger")
+wn.bgcolor("dark grey")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Main game loop
+while True:
+    wn.update()
+
+# Paddle A
+
+
+
+
+# Paddle B
+
+
+# tbc....
+
+    
