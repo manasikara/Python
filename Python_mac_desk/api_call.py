b8d9bfae817c235466c793d4e5975a4ac1590b06
manasikara <slawektatinger@me.com>
2023-08-09 16:31:25 +0200

    ...

--- Python_mac_desk/api_call.py
+++
@@ -1,4 +0,0 @@
-import requests
-response = requests.get("https://reqres.in/api/users/2")
-print(response.text, response.status_code)
-
--- README.md
+++
@@ -1,7 +0,0 @@
-# Python
- Learnig Python
-
-1st project: A simple 'quiz game'
-
-2nd project: Number Guesser ---> to be continued
-
