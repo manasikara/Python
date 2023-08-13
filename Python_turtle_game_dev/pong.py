151434fbd16578b1659bb89d9334ec8c830b3d9c
slawomir_tatinger <59393759+manasikara@users.noreply.github.com>
2023-08-13 23:29:02 +0200

    ...

--- Python_turtle_game_dev/pong.py
+++ Python_turtle_game_dev/pong.py
@@ -12,3 +12,22 @@ HEIGHT = (600)
 
 pygame.init()
 game_font = pygame.font.SysFont('Ubuntu', 40)
+
+delay = 30
+paddle_speed = 20
+paddle_width = 10
+paddle_height = 100
+
+p1_x_position = 10
+p1_y_position = HEIGHT / 2 - paddle_height / 2
+
+p2_x_position = WIDTH - paddle_width - 10
+p2_y_position = HEIGHT / 2 - paddle_height / 2
+
+pl1_score = 0
+pl2_score = 0
+
+p1_up = False
+p1_down = False
+p2_up = False
+p2_down = False
