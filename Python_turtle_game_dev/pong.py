aaad45a63edf0633f0342274d42f0e4f7bd770bc
slawomir_tatinger <59393759+manasikara@users.noreply.github.com>
2023-08-13 23:58:02 +0200

    ...

--- Python_turtle_game_dev/pong.py
+++ Python_turtle_game_dev/pong.py
@@ -24,10 +24,16 @@ p1_y_position = HEIGHT / 2 - paddle_height / 2
 p2_x_position = WIDTH - paddle_width - 10
 p2_y_position = HEIGHT / 2 - paddle_height / 2
 
-pl1_score = 0
-pl2_score = 0
+p1_score = 0
+p2_score = 0
 
 p1_up = False
 p1_down = False
 p2_up = False
 p2_down = False
+
+ball_x_pos = WIDTH / 2
+ball_y_pos = HEIGHT / 2
+nall_width = 8
+ball_x_vel = -10
+ball_y_vel = 0
