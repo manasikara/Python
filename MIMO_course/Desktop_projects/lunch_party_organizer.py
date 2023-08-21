b3cc5126c84377104c11189d6f5c0eb96931e716
slawomir_tatinger <59393759+manasikara@users.noreply.github.com>
2023-08-21 09:47:44 +0200

    mimo_desktop_prjects

---
+++ MIMO_course/Desktop_projects/lunch_party_organizer.py
@@ -0,0 +1,12 @@
+menu_item = "pizza"
+guests = 10 + 1
+menu_item=("biryani")
+biryani_per_person = 1
+biryani_needed = biryani_per_person * guests
+biryani_prepared = 10
+enough_biryani = biryani_prepared == biryani_needed
+biryani_per_guest = biryani_prepared / guests
+print(guests)
+print(menu_item)
+print("Updated menu item is: " +menu_item)
+print(biryani_per_guest)
