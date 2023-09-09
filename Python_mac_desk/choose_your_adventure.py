cd0d155f5cba45cf85fb787db079de3f2e350014
manasikara <slawektatinger@me.com>
2023-09-09 10:44:19 +0200

    a new game in dev

---
+++ Python_mac_desk/choose_your_adventure.py
@@ -0,0 +1,7 @@
+# learning source --> https://www.youtube.com/watch?v=DLn3jOsNRVE&list=PLE3yaxY1qUqOg2_EV35loEGXnC8nuSbrn&index=12
+
+name = input("Type your name:")
+print = input("Welcome", name, "to this adventure!")
+answer = input("You're on a dirt road, it has come to an end and you can go left or right. Which way would you like to go?")
+
+#tbc...
