97e37112b03f6d484c68c21e56e5ce17ac8bf0fd
slawomir_tatinger <59393759+manasikara@users.noreply.github.com>
2023-09-10 09:01:16 +0200

    adventure game updated

--- Python_mac_desk/choose_your_adventure.py
+++ Python_mac_desk/choose_your_adventure.py
@@ -1,7 +1,21 @@
 # learning source --> https://www.youtube.com/watch?v=DLn3jOsNRVE&list=PLE3yaxY1qUqOg2_EV35loEGXnC8nuSbrn&index=12
 
 name = input("Type your name:")
-print = input("Welcome", name, "to this adventure!")
-answer = input("You're on a dirt road, it has come to an end and you can go left or right. Which way would you like to go?")
+print = input("Welcome", name, "to this pixel adventure!. Use the capitalised words as an answer, please.")
+answer = input("You're in your room, in front of your PC. The PC is turned off. Do you want to START it or LEAVE the room in order to do some useless activities?")
 
-#tbc...
+if answer == "start":
+    answer = input("A beautifull beep sound had been played and you see the 'welcome' screen on the monitor. Do you want to LOG-IN, or SHUTDOWN?")
+    if answer == "log-in":
+        print()
+    elif answer == "shutdown":
+      print()
+    else:
+        print("Not a valid option, looser!")    
+            
+
+elif answer == "leave":
+    print()
+
+else:
+    print("Not a valid option, looser!")    
