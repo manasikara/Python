c73262261b327a5cb3c231a7b5f36e51fc5decb0
slawomir_tatinger <59393759+manasikara@users.noreply.github.com>
2023-09-10 14:49:07 +0200

    tbc...

--- Python_mac_desk/choose_your_adventure.py
+++ Python_mac_desk/choose_your_adventure.py
@@ -1,21 +1,24 @@
 # learning source --> https://www.youtube.com/watch?v=DLn3jOsNRVE&list=PLE3yaxY1qUqOg2_EV35loEGXnC8nuSbrn&index=12
 
-name = input("Type your name:")
-print = input("Welcome", name, "to this pixel adventure!. Use the capitalised words as an answer, please.")
-answer = input("You're in your room, in front of your PC. The PC is turned off. Do you want to START it or LEAVE the room in order to do some useless activities?")
+name = input("Type your name: ")
+
+print = input("Welcome " + name + " to this Pixel Adventure!. Use the capitalised words as an answer")
+
+answer = input("You're in your room, in front of your PC. The PC is turned off. Do you want to START it or LEAVE the room in order to do some useless activities? ").lower()
 
 if answer == "start":
-    answer = input("A beautifull beep sound had been played and you see the 'welcome' screen on the monitor. Do you want to LOG-IN, or SHUTDOWN?")
+    answer = input("A beautifull beep sound had been played and you see the 'Welcome to the Pixel World' writing on the screen. Do you want to LOG-IN, or SHUTDOWN?")
+    
     if answer == "log-in":
-        print()
+        print("Start exploring! + name")
     elif answer == "shutdown":
-      print()
+      print("Bye + name")
     else:
         print("Not a valid option, looser!")    
             
 
 elif answer == "leave":
-    print()
+    print("You really mus'nt like the Pixel World. Bye.")
 
 else:
     print("Not a valid option, looser!")    
